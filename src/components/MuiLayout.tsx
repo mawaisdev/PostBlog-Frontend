45fd4868dd8503f3a1aa34cb3a298a0c422b7cb3
Muhammad Awais <m.awais2030@gmail.com>
2023-10-11 14:35:42 +0500

    MuiBox

---
+++ src/components/MuiLayout.tsx
@@ -0,0 +1,30 @@
+import { Box } from '@mui/material'
+
+export const MuiLayout = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: 'primary.main',
+          color: 'whitesmoke',
+          height: '100px',
+          width: '100px',
+          padding: '16px',
+          '&:hover': {
+            backgroundColor: 'primary.light',
+          },
+        }}
+      >
+        Hello
+      </Box>
+
+      <Box
+        display={'flex'}
+        height={'100px'}
+        width={'100px'}
+        bgcolor='success.light'
+        p={2}
+      ></Box>
+    </>
+  )
+}
