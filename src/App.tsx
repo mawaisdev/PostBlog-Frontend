5022da61f9dbd4abc4d2d48980280b699870c936
Muhammad Awais <m.awais2030@gmail.com>
2023-10-13 12:46:28 +0500

    Refactered branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,5 @@
-import { MuiResponsiveness } from './components/MuiResponsiveness'
-import { createTheme, colors, ThemeProvider } from '@mui/material'
-
-const theme = createTheme({
-  status: {
-    danger: '#e53e3e',
-  },
-  palette: {
-    secondary: {
-      main: colors.orange[500],
-    },
-    neutral: {
-      main: colors.blueGrey[500],
-      darker: colors.blueGrey[900],
-    },
-  },
-})
-
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div className='App'>
-        <MuiResponsiveness />
-      </div>
-    </ThemeProvider>
-  )
+  return <div className='App'>Hello</div>
 }
 
 export default App
