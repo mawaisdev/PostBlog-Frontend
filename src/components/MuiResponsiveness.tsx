9db5686c32c98b3d369a491b787894ada2fc4bd6
Muhammad Awais <m.awais2030@gmail.com>
2023-10-13 12:32:23 +0500

    MuiThemeCustomization

--- src/components/MuiResponsiveness.tsx
+++ src/components/MuiResponsiveness.tsx
@@ -1,24 +1,50 @@
-import { Box } from '@mui/material'
+import { Box, styled } from '@mui/material'
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  height: '250px',
+  width: '250px',
+  backgroundColor: theme.palette.neutral?.darker,
+}))
 
 export const MuiResponsiveness = () => {
   return (
-    <Box
-      sx={{
-        height: '300px',
-        width: {
-          xs: 100, // 0
-          sm: 200, // 600
-          md: 300, // 900
-          lg: 400, // 1200
-          xl: 500, // 1536
-        },
-        bgcolor: 'primary.main',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      Box
-    </Box>
+    <>
+      <Box
+        sx={{
+          height: '300px',
+          width: {
+            xs: 100, // 0
+            sm: 200, // 600
+            md: 300, // 900
+            lg: 400, // 1200
+            xl: 500, // 1536
+          },
+          bgcolor: 'secondary.main',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        Box
+      </Box>
+
+      <StyledBox
+        sx={{
+          height: '300px',
+          width: {
+            xs: 100, // 0
+            sm: 200, // 600
+            md: 300, // 900
+            lg: 400, // 1200
+            xl: 500, // 1536
+          },
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        Box
+      </StyledBox>
+    </>
   )
 }
