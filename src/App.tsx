deed4c43e2bda47bdea6bd0ed17fbb6fc760287c
Muhammad Awais <m.awais2030@gmail.com>
2023-10-13 12:39:15 +0500

    With Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { MuiResponsiveness } from './components/MuiResponsiveness'
+import { createTheme, colors, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    secondary: {
+      main: colors.orange[500],
+    },
+    neutral: {
+      main: colors.blueGrey[500],
+      darker: colors.blueGrey[900],
+    },
+  },
+})
+
 function App() {
-  return <h1>Hello!</h1>
+  return (
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <MuiResponsiveness />
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App
