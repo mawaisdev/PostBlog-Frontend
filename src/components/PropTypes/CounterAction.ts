dabd42a5f0950922889eba468fd2d1c87fb1f99f
Muhammad Awais <m.awais2030@gmail.com>
2023-10-16 12:15:46 +0500

    useReducer Strict Action Types

--- src/components/PropTypes/CounterAction.ts
+++ src/components/PropTypes/CounterAction.ts
@@ -1,4 +1,10 @@
-export interface CounterAction {
-  type: string
+export interface UpdateAction {
+  type: 'increment' | 'decrement'
   payload: number
 }
+
+export interface ResetAction {
+  type: 'reset'
+}
+
+export type CounterAction = UpdateAction | ResetAction
--- src/components/States/Counter.tsx
+++ src/components/States/Counter.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Stack } from '@mui/material'
+import { Box, Button, Paper, Stack } from '@mui/material'
 import { useReducer } from 'react'
 import { CounterState } from '../PropTypes/CounterState'
 import { CounterAction } from '../PropTypes/CounterAction'
@@ -11,6 +11,8 @@ function reducer(state: CounterState, action: CounterAction) {
       return { count: state.count + action.payload }
     case 'decrement':
       return { count: state.count - action.payload }
+    case 'reset':
+      return initialState
     default:
       return state
   }
@@ -20,20 +22,40 @@ export const Counter = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
-    <Stack direction={'row'} spacing={2}>
-      <Button
-        variant={'contained'}
-        onClick={() => dispatch({ type: 'increment', payload: 10 })}
-      >
-        Increment 10
-      </Button>
-      <Box sx={{ paddingTop: '10px' }}>{state.count}</Box>
-      <Button
-        variant={'contained'}
-        onClick={() => dispatch({ type: 'decrement', payload: 10 })}
-      >
-        Decrement 10
-      </Button>
-    </Stack>
+    <Paper
+      sx={{
+        width: '400px',
+        height: '400px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Stack spacing={2}>
+        <Stack direction={'row'} spacing={2}>
+          <Button
+            variant={'contained'}
+            onClick={() => dispatch({ type: 'increment', payload: 10 })}
+          >
+            Increment 10
+          </Button>
+          <Box sx={{ paddingTop: '10px' }}>{state.count}</Box>
+          <Button
+            variant={'contained'}
+            onClick={() => dispatch({ type: 'decrement', payload: 10 })}
+          >
+            Decrement 10
+          </Button>
+        </Stack>
+        <Stack>
+          <Button
+            variant='contained'
+            onClick={() => dispatch({ type: 'reset' })}
+          >
+            Reset
+          </Button>
+        </Stack>
+      </Stack>
+    </Paper>
   )
 }
