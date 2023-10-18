90876f793cee8886847b2c0f667437345f3bea20
Muhammad Awais <m.awais2030@gmail.com>
2023-10-18 18:08:34 +0500

    Routing Updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Home } from './Pages/Home'
 import SignUpPage from './Pages/Signup'
 import LoginPage from './Pages/Login'
 import NotFound404 from './Pages/Missing'
-import { RequireAuth } from './Components/RequireAuth'
+import { RequireAuth, RequireNoAuth } from './Components/RequireAuth'
 import Layout from './Routes/Layout'
+import { Dashboard } from './Pages/DashboardPage'
 
 const App = () => {
   return (
@@ -12,12 +13,14 @@ const App = () => {
       <Route path='/' element={<Layout />}>
         {/* Public Routes Here*/}
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
+        <Route element={<RequireNoAuth />}>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+        </Route>
 
         {/* Private Routes Here*/}
         <Route element={<RequireAuth />}>
-          <Route path='/dashboard' element={<div>Dashboard</div>} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Route>
 
         {/* Errors Routes Here*/}
--- src/Components/RequireAuth.tsx
+++ src/Components/RequireAuth.tsx
@@ -12,3 +12,14 @@ export const RequireAuth = () => {
     <Navigate to='/login' state={{ from: location }} replace />
   )
 }
+
+export const RequireNoAuth = () => {
+  const { token } = useAuth()
+  const location = useLocation()
+
+  return !token ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/' state={{ from: location }} replace />
+  )
+}
