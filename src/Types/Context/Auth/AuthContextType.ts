34d5d1ba9711644d6062e7d2e747799c5ed2a9e2
Muhammad Awais <m.awais2030@gmail.com>
2023-10-19 19:17:21 +0500

    AuthContext Type

--- src/Types/Context/Auth/AuthContextType.ts
+++ src/Types/Context/Auth/AuthContextType.ts
@@ -2,4 +2,6 @@ import { AuthState } from './AuthState'
 
 export interface AuthContextType extends AuthState {
   setAuthState: React.Dispatch<React.SetStateAction<AuthState>>
+  persistState: boolean
+  setPersistState: React.Dispatch<React.SetStateAction<boolean>>
 }
