54dcf56ef07188fa9b282cd3f6b8c44011171a00
Muhammad Awais <m.awais2030@gmail.com>
2023-10-20 13:12:29 +0500

    Protected Settings for React Dev Tools

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,11 @@ import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './Contexts/AuthContext.tsx'
 
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools()
+}
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
