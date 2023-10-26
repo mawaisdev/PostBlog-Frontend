f8d84337e5dfc00111c38ea9aa315a79006e80fa
Muhammad Awais <m.awais2030@gmail.com>
2023-10-26 17:31:38 +0500

    Pagination In Posts

--- src/Components/PostCard.tsx
+++ src/Components/PostCard.tsx
@@ -37,13 +37,16 @@ export function PostCard(post: Post) {
       <CardOverflow variant='soft' sx={{ bgcolor: 'background.level1' }}>
         <Divider inset='context' />
         <CardContent orientation='horizontal'>
-          <Typography
-            level='body-xs'
-            fontWeight='md'
-            textColor='text.secondary'
-          >
-            Author: {post.user.userName}
-          </Typography>
+          <Link to={`/users/${post.user.id}`} className='noUnderline'>
+            <Typography
+              level='body-xs'
+              fontWeight='md'
+              textColor='text.secondary'
+            >
+              Author: {post.user.userName}
+            </Typography>
+          </Link>
+
           <Divider orientation='vertical' />
           <Typography
             level='body-xs'
@@ -57,48 +60,3 @@ export function PostCard(post: Post) {
     </Card>
   )
 }
-
-// import AspectRatio from '@mui/joy/AspectRatio'
-// import Card from '@mui/joy/Card'
-// import CardContent from '@mui/joy/CardContent'
-// import CardOverflow from '@mui/joy/CardOverflow'
-// import Divider from '@mui/joy/Divider'
-// import Typography from '@mui/joy/Typography'
-// import Link from '@mui/joy/Link'
-// import { Post } from '../Types/Responses/Post/GetAllPostsResponse'
-
-// export function PostCard(post: Post) {
-//   return (
-//     <Card variant='outlined' sx={{ width: '100%' }}>
-//       <CardOverflow>
-//         <AspectRatio ratio='2'>
-//           <img
-//             src='https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318'
-//             srcSet='https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x'
-//             loading='lazy'
-//             alt=''
-//           />
-//         </AspectRatio>
-//       </CardOverflow>
-//       <CardContent>
-//         <Typography level='title-md'>
-//           <Link href='#multiple-actions' overlay underline='none'>
-//             Yosemite National Park
-//           </Link>
-//         </Typography>
-//         <Typography level='body-sm'>
-//           <Link href='#multiple-actions'>California</Link>
-//         </Typography>
-//       </CardContent>
-
-//       <CardOverflow variant='soft'>
-//         <Divider inset='context' />
-//         <CardContent orientation='horizontal'>
-//           <Typography level='body-xs'>6.3k views</Typography>
-//           <Divider orientation='vertical' />
-//           <Typography level='body-xs'>1 hour ago</Typography>
-//         </CardContent>
-//       </CardOverflow>
-//     </Card>
-//   )
-// }
--- src/Pages/PostsPage.tsx
+++ src/Pages/PostsPage.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material'
+import { Box, Grid, Pagination } from '@mui/material'
 import { useEffect, useState } from 'react'
 import axios from '../Api/axios'
 import { GetAllPostsResponse } from '../Types/Responses/Post/GetAllPostsResponse'
@@ -7,13 +7,21 @@ import { AxiosError } from 'axios'
 
 export const PostsPage = () => {
   const [allPosts, setAllPosts] = useState<GetAllPostsResponse>()
+  const [page, setPage] = useState(1)
+  const pageSize = 10
 
   useEffect(() => {
     let isMounted = true
     const conntroller = new AbortController()
+    const skipValue = (page - 1) * pageSize
+    const takeValue = pageSize
     const getAllPosts = async () => {
       try {
         const { data } = await axios.get('/posts', {
+          params: {
+            skip: skipValue,
+            take: takeValue,
+          },
           signal: conntroller.signal,
         })
         isMounted && setAllPosts(data)
@@ -27,23 +35,24 @@ export const PostsPage = () => {
       isMounted = false
       conntroller.abort()
     }
-  }, [])
+  }, [page, pageSize])
   return (
-    <Grid container spacing={1} m={2}>
-      {allPosts?.data?.map((post) => (
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          md={3}
-          lg={3}
-          xl={2}
-          key={post.id}
-          style={{ flex: '1 0 auto' }}
-        >
-          <PostCard {...post} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid container spacing={1} mt={2}>
+        {allPosts?.data?.map((post) => (
+          <Grid item xs={12} sm={4} md={3} lg={3} xl={3} key={post.id}>
+            <PostCard {...post} />
+          </Grid>
+        ))}
+      </Grid>
+
+      <Box display='flex' justifyContent='center' mt={4}>
+        <Pagination
+          count={Math.ceil(allPosts ? allPosts.totalPostsCount / pageSize : 0)}
+          page={page}
+          onChange={(_, value) => setPage(value)}
+        />
+      </Box>
+    </>
   )
 }
