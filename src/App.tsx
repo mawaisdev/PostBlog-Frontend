a97c2ed19265223a5a0fe53ac2b556bb200132b3
Muhammad Awais <m.awais2030@gmail.com>
2023-10-27 14:31:50 +0500

    Refactored Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import { NotFound404 } from './Pages/NotFound404Page'
 import { NoAuth, RequireAuth } from './Components/ControllAuthRoutes'
 import { Layout } from './Routes/Layout'
@@ -12,47 +12,66 @@ import { SignUpPage } from './Pages/SignUpPage'
 import { CategoriesPage } from './Pages/CategoriesPage'
 import { PostsPage } from './Pages/PostsPage'
 import { Roles } from './Types/Responses/User'
+import { SideNav } from './Components/SideNav'
+import { AuthProvider } from './Contexts/AuthContext'
+import { CategoryProvider } from './Contexts/CategoryContext'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        {/* Public Routes */}
-        <Route path='/' element={<PostsPage />} />
-        <Route path='/login' element={<NoAuth />}>
-          <Route index element={<LoginPage />} />
-        </Route>
-        <Route path='/signup' element={<NoAuth />}>
-          <Route index element={<SignUpPage />} />
-        </Route>
+    <AuthProvider>
+      <CategoryProvider>
+        <Router>
+          <SideNav>
+            <Routes>
+              <Route path='/' element={<Layout />}>
+                {/* Public Routes */}
+                <Route path='/' element={<PostsPage />} />
+                <Route path='/login' element={<NoAuth />}>
+                  <Route index element={<LoginPage />} />
+                </Route>
+                <Route path='/signup' element={<NoAuth />}>
+                  <Route index element={<SignUpPage />} />
+                </Route>
 
-        {/* Routes for Authenticated Users (Protected by PersistLogin) */}
-        <Route element={<PersistLogin />}>
-          {/* Routes for Admin */}
-          <Route element={<RequireAuth roles={[Roles.Admin]} />}>
-            <Route path='/category' element={<CategoriesPage />} />
-          </Route>
+                {/* Routes for Authenticated Users (Protected by PersistLogin) */}
+                <Route element={<PersistLogin />}>
+                  {/* Routes for Admin */}
+                  <Route element={<RequireAuth roles={[Roles.Admin]} />}>
+                    <Route path='/category' element={<CategoriesPage />} />
+                  </Route>
 
-          {/* Routes for User */}
-          <Route element={<RequireAuth roles={[Roles.User]} />}>
-            <Route path='/userprofile' element={<h1>Profile for User</h1>} />{' '}
-            {/* Renamed to clarify it's for users */}
-          </Route>
+                  {/* Routes for User */}
+                  <Route element={<RequireAuth roles={[Roles.User]} />}>
+                    <Route
+                      path='/userprofile'
+                      element={<h1>Profile for User</h1>}
+                    />{' '}
+                    {/* Renamed to clarify it's for users */}
+                  </Route>
 
-          {/* Routes for Both Admin and User */}
-          <Route element={<RequireAuth roles={[Roles.Admin, Roles.User]} />}>
-            <Route path='/dashboard' element={<DashboardPage />} />
-            <Route path='/profile' element={<ProfilePage />} />
-            <Route path='/settings' element={<SettingsPage />} />
-            <Route path='/unauthorized' element={<UnauthorizedPage />} />
-            <Route path='/posts/:id' element={<h1>Post Page</h1>} />
-          </Route>
-        </Route>
-      </Route>
+                  {/* Routes for Both Admin and User */}
+                  <Route
+                    element={<RequireAuth roles={[Roles.Admin, Roles.User]} />}
+                  >
+                    <Route path='/dashboard' element={<DashboardPage />} />
+                    <Route path='/profile' element={<ProfilePage />} />
+                    <Route path='/settings' element={<SettingsPage />} />
+                    <Route
+                      path='/unauthorized'
+                      element={<UnauthorizedPage />}
+                    />
+                    <Route path='/posts/:id' element={<h1>Post Page</h1>} />
+                  </Route>
+                </Route>
+              </Route>
 
-      {/* Error Routes */}
-      <Route path='*' element={<NotFound404 />} />
-    </Routes>
+              {/* Error Routes */}
+              <Route path='*' element={<NotFound404 />} />
+            </Routes>
+          </SideNav>
+        </Router>
+      </CategoryProvider>
+    </AuthProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,37 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './Contexts/AuthContext.tsx'
-
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
-// import Navbar from './Components/Navbar.tsx'
-import { SideNav } from './Components/SideNav.tsx'
-import { Box } from '@mui/material'
-import { CategoryProvider } from './Contexts/CategoryContext.tsx'
 
 if (process.env.NODE_ENV === 'production') {
   disableReactDevTools()
 }
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <CategoryProvider>
-          <SideNav>
-            <Routes>
-              <Route
-                path='/*'
-                element={
-                  <Box height={100}>
-                    <App />
-                  </Box>
-                }
-              />
-            </Routes>
-          </SideNav>
-        </CategoryProvider>
-      </AuthProvider>
-    </Router>
+    <App />
   </React.StrictMode>
 )
